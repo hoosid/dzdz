12c5d8df00e965c2dbb4f974f038307fcedc237d
sidaxe pro <108492494+hoosid@users.noreply.github.com>
2024-08-26 19:40:17 +0100

    Update homepage.js

--- routes/homepage.js
+++ routes/homepage.js
@@ -11,7 +11,7 @@ const upload = multer({ storage: storage });
 /* GET home page. */
 
 
-router.get('/home', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
       const admin = await prisma.admin.findMany(); 
       res.render('home', { admin: admin });
